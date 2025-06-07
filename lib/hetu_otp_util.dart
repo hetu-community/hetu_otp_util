e1450c123f1ea84bc3b51e58d24750cc5aaf4a2f
Kingkor Roy Tirtho <krtirtho@gmail.com>
2025-06-07 14:19:08 +0600

    docs: add doc comments for flutter bytecode loader

--- lib/hetu_otp_util.dart
+++ lib/hetu_otp_util.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/services.dart';
 import 'package:hetu_script/hetu/hetu.dart';
 import 'package:path/path.dart';
 
@@ -8,15 +9,27 @@ abstract class HetuOtpUtilLoader {
     Hetu hetu,
     String packagePath,
   ) async {
-    final byteCodeFile = File(join(packagePath, 'lib/assets/bytecode/otp_util.out'));
+    final byteCodeFile = File(
+      join(packagePath, 'lib/assets/bytecode/otp_util.out'),
+    );
     final byteCode = await byteCodeFile.readAsBytes();
 
     hetu.loadBytecode(bytes: byteCode, moduleName: 'otp_util');
   }
 
+  /// Loads the bytecode for the OTP utility from the Flutter asset bundle.
+  /// Add following to your `pubspec.yaml`:
+  /// 
+  /// ```yaml
+  /// flutter:
+  ///   assets:
+  ///     - packages/hetu_otp_utl/assets/bytecode/otp_util.out
+  /// ```
   static Future<void> loadBytecodeFlutter(Hetu hetu) async {
-    final byteCodeFile = File('packages/hetu_otp_utl/assets/bytecode/otp_util.out');
-    final byteCode = await byteCodeFile.readAsBytes();
+    final byteCodeFile = await rootBundle.load(
+      'packages/hetu_otp_utl/assets/bytecode/otp_util.out',
+    );
+    final byteCode = byteCodeFile.buffer.asUint8List();
 
     hetu.loadBytecode(bytes: byteCode, moduleName: 'otp_util');
   }
